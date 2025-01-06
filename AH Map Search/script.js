77e2dd8f87728f95f895e2ee3c55bf81ddf93c4a
Casper Post <casperaronpost@protonmail.com>
2025-01-06 01:30:15 +0100

    Functionality Almost Working! (except for timer)

--- AH Map Search/script.js
+++ AH Map Search/script.js
@@ -20,10 +20,10 @@ const getLocations = async function () {
  */
 const initMap = async function () {
   /** Init Leaflet instance */
-  const map = L.map("map", { attributionControl: false }).setView(
-    [52.154912, 5.386841],
-    7
-  );
+  const map = L.map("map", {
+    attributionControl: false,
+    zoomControl: false,
+  }).setView([52.154912, 5.386841], 7);
   /** Set Leaflet map style  */
   L.tileLayer(
     "https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png",
@@ -79,6 +79,37 @@ const askConformation = function (answer, answerLayer) {
   }).openOn(answerLayer);
 };
 
+const setAhText = function (text, location) {
+  const city = location.tag.filter((item) => item._k === "addr:city")[0]._v;
+  const street = location.tag.filter((item) => item._k === "addr:street")[0]._v;
+  const houseNumber = location.tag.filter(
+    (item) => item._k === "addr:housenumber"
+  )[0]._v;
+
+  text.textContent = `${street} ${houseNumber}, ${city} `;
+};
+
+const drawLineBetweenPoints = function (m1, m2, layer) {
+  // Get start and end points of the line
+  const lineCoords = [m1.getLatLng(), m2.getLatLng()];
+  // Draw line
+  L.polyline(lineCoords, {
+    color: "var(--line-color)",
+    weight: 2,
+  }).addTo(layer);
+};
+
+const zoomMapToMarkers = function (m1, m2, map) {
+  // Methode 2: Met de coördinaten
+  const bounds = L.latLngBounds([m1.getLatLng(), m2.getLatLng()]);
+  map.fitBounds(bounds, {
+    padding: [300, 100],
+    maxZoom: 17,
+    animate: true,
+    duration: 1.5,
+  });
+};
+
 /**
  * Main function that is running during the game. Only resolves
  * after 10 rounds.
@@ -87,55 +118,121 @@ const askConformation = function (answer, answerLayer) {
  * @returns The score that the player got
  */
 const playGame = async function (allLocations, map) {
-  /** Create a copy of locations array */
-  const locations = allLocations.osm.node.slice();
-  let round = 1;
-  let location = getRandomAh(locations);
-  let clickedLocation;
-  let score = 0;
-  const answerLayer = L.layerGroup().addTo(map);
-  let confirmed = false;
+  /** Create neccesary variables */
+  const locations = allLocations.osm.node.slice(); // Locations copy
+  let round = 1; // round counter
+  let location = getRandomAh(locations); // AH location (changes every round)
+  let clickedLocation; // The current clicked location on the map
+  let score = 0; // The score (adds up every round)
+  const answerLayer = L.layerGroup().addTo(map); // Layer with the markers
+  let disableMapClick = false; // true if user confirmed answer
+  let nextBtnActive = false; // true when next button is active
+  const ahTitle = document.getElementById("ah-address"); // DOM-element text
+  const result = document.getElementById("distanceScore"); // DOM-element score
+  const counter = document.getElementById("counter");
+  const nextBtn = document.getElementById("continueBtn");
+  // The custom AH location pin
+  const ahIcon = L.icon({
+    iconUrl: "AH_pin.png",
+    iconSize: [40, 45], // width: 38px, height: 40px
+    iconAnchor: [20, 45], // x: half of width, y: full height for bottom point
+  });
+  // The custom user location pin
+  const userIcon = L.icon({
+    iconUrl: "user_pin.png",
+    iconSize: [40, 45], // width: 38px, height: 40px
+    iconAnchor: [20, 45], // x: half of width, y: full height for bottom point
+  });
 
   /** 1. Set first location and start timer. */
+  setAhText(ahTitle, location);
 
   /** 2. Set a pin on a location when clicked */
   map.on("click", (e) => {
-    if (!confirmed) {
+    if (!disableMapClick) {
       clickedLocation = e.latlng;
       askConformation(e.latlng, answerLayer);
     }
   });
 
-  /** 3. Add Event handler for conformation clicks */
-  const mapDiv = document.getElementById("map");
-  mapDiv.addEventListener("click", (e) => {
-    if (e.target.id === "confirm-btn") {
-      confirmed = true;
-      /* Verwerk antwoord: */
-      /* 1. Set pin voor gegokte locatie op map */
-
-      /* 2. set pin voor daadwerkelijke locatie */
-      /* 3. bereken afstand */
-      /* 4. visualiseer afstand met cirkel */
-      /* 5. Bereken score, set naar score variabele, increase round */
-      /* reset:
-      6. verwijder alle markers */
-      /* 7. reset zoom */
-      /* 8. Nieuwe AH-locatie ophalen */
-      /* 9. timer resetten (extra) */
-      confirmed = false;
-    }
-
-    /** Extra: handle timer */
-    /** Extra: handle highscores */
-  });
-
-  // Return a Promise that resolves after 10 rounds (clicks)
   return new Promise((resolve) => {
-    if (round >= 11) {
-      map.off();
-      resolve(score);
-    }
+    /** 3. Add Event handler for confirmation clicks */
+    const mapDiv = document.getElementById("map");
+    mapDiv.addEventListener("click", (e) => {
+      if (e.target.id === "confirm-btn") {
+        disableMapClick = true;
+        /* 1. Remove confirmation pop up and set pin for clicked location */
+        answerLayer.clearLayers();
+        const userPin = L.marker([clickedLocation.lat, clickedLocation.lng], {
+          icon: userIcon,
+        }).addTo(answerLayer);
+
+        /* 2. Set pin for the Albert Heijn location */
+        const ahPin = L.marker([location._lat, location._lon], {
+          icon: ahIcon,
+        }).addTo(answerLayer);
+
+        /* 3. Calculate distance in KM */
+        const distanceKM = Math.floor(
+          userPin.getLatLng().distanceTo(ahPin.getLatLng()) / 1000
+        );
+
+        /** 4. Calculate score. User only earns points if the answer
+         * is within 100km of the actual location. 100 points is the perfect
+         * answer. For every kilomter distance, 1 point gets subtracted,
+         * until 0 remains.
+         */
+        if (distanceKM < 100) {
+          score += 100 - distanceKM;
+        }
+
+        /* 5. Visualize with line and set distance text */
+        drawLineBetweenPoints(userPin, ahPin, answerLayer, distanceKM);
+        result.textContent = `Afstand: ${distanceKM}KM (score +${
+          distanceKM < 100 ? 100 - distanceKM : "0"
+        })`;
+
+        /* 7. Zoom out to correct boundaries */
+        zoomMapToMarkers(userPin, ahPin, map);
+
+        /* 9. Hide counter & show continue button */
+        nextBtn.classList.remove("hidden");
+        counter.classList.add("hidden");
+        nextBtnActive = true;
+      }
+
+      /** Trigger this event if the continue button is clicked */
+      if (e.target.closest("#rightBox") && nextBtnActive) {
+        /* 10. Remove all markers and line */
+        answerLayer.clearLayers();
+        /* 11. reset zoom */
+        map.flyTo([52.154912, 5.386841], 7, {
+          duration: 0.5,
+        });
+        /* 12. Get new location and show text */
+        location = getRandomAh(locations);
+        setAhText(ahTitle, location);
+        result.textContent = " ";
+        /* 13. Hide continue button, show timer */
+        nextBtn.classList.add("hidden");
+        counter.classList.remove("hidden");
+        nextBtnActive = false;
+
+        /* 14. Enable map click */
+        disableMapClick = false;
+
+        /* 8. Increase round */
+        if (++round >= 11) {
+          map.off();
+          resolve(score);
+        }
+      }
+
+      /** Extra: handle timer */
+      /** Extra: handle highscores */
+    });
+
+    // Return a Promise that resolves after 10 rounds (clicks)
   });
 };
 
