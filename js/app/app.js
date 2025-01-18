255d0c8566b446141dd4a89f770d04ad4693503a
Casper Post <89775408+The-Friendly-Ghost@users.noreply.github.com>
2025-01-18 09:03:16 +0100

    Update app.js

--- js/app/app.js
+++ js/app/app.js
@@ -1,6 +1,8 @@
 "use strict";
 
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from "../supabaseConfig.js";
+// import { SUPABASE_URL, SUPABASE_ANON_KEY } from "../supabaseConfig.js";
+const supabaseUrl = process.env.SUPABASE_URL  
+const supabaseKey = process.env.SUPABASE_ANON_KEY
 import { Game } from "../game/game.js";
 import { Player } from "../player/player.js";
 import { Music } from "../game/music.js";
